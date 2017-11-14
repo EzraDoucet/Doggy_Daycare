ab1e6af7f7ed26417b2738e9aec14cbf83f6d261
Ezra <ezra.doucet@gmail.com>
2017-11-14 09:47:12 -0600

    Add update/edit method, view, and controller.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,12 +1,15 @@
 class DogsController < ApplicationController
 
+ before_action :set_dog, only: [:show, :edit, :update]
+ # Runs set_dog before only the show, edit, and update methods
+
  def index
   @dogs = Dog.all
 
  end
 
  def new
-  @new_dog = Dog.new
+  @dog = Dog.new
  end
 
  def create
@@ -14,18 +17,33 @@ class DogsController < ApplicationController
 
   # if saves, go to index w/ notification
   if @dog.save
-   redirect_to dogs_url, notice: "Sucessfully created a dog."
+   redirect_to dogs_url, notice: "Sucessfully created #{@dog.name}."
 
   else
   # if doesn't save, re-render the new dog form
-   flash.now[:notice] = "Something went wrong.  We couldn't save your dog..."
+   flash.now[:notice] = "Something went wrong.  We couldn't save #{@dog.name}..."
    render :new
   end
  end
 
 
  def show
-  @dog = Dog.find(params[:id])
+  # set_dog
+ end
+
+ def edit
+  # set_dog
+ end
+
+ def update
+  # set_dog
+
+  if @dog.update(dog_params)
+   redirect_to dog_url(@dog), notice: "Successfully updated #{@dog.name}."
+  else
+   flash.now[:notice] = "Something went wrong.  We couldn't update #{@dog.name}..."
+   render :edit
+  end
  end
 
  private
@@ -34,6 +52,8 @@ class DogsController < ApplicationController
   params.require(:dog).permit(:name, :age, :breed)
  end
 
-
+ def set_dog
+  @dog = Dog.find(params[:id])
+ end
 
 end
